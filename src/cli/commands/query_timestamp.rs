5085e2e1bbef06a5dc3381e35348250427505fbc
silfoxs <songwenwen@outlook.com>
2024-07-19 15:50:27 +0800

    to query local timestamp

--- src/cli/commands/query_timestamp.rs
+++ src/cli/commands/query_timestamp.rs
@@ -1,9 +1,9 @@
 extern crate chrono;
-use chrono::Utc;
+use chrono::{Local};
 
 pub fn handle() {
     // 获取当前时间
-    let now = Utc::now();
+    let now = Local::now();
 
     // 获取当前时间戳（秒）
     let timestamp = now.timestamp();
@@ -11,6 +11,9 @@ pub fn handle() {
     // 获取当前时间戳（毫秒）
     let timestamp_millis = now.timestamp_millis();
 
+    let datetime = format!("{}", now.format("%Y-%m-%d %H:%M:%S"));
+
     println!("当前时间戳（秒）：{}", timestamp);
     println!("当前时间戳（毫秒）：{}", timestamp_millis);
+    println!("当前时间：{}", datetime);
 }
